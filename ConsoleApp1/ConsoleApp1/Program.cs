2296371f0a013872e2fe4feaec109e2406149758
Sasha <natson6221@mail.ru>
2019-04-13 18:51:01 +0300

    paha2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,12 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace l1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Kvit();
+            Console.ReadKey();
+        }
+
+
+        static void Kvit()
+        {
+            Kvit kvit = new Kvit();
+            kvit.SetInfo();
+            kvit.GetInfo();
+            Console.WriteLine();
+
+
+        }
+    }
+    class Kvit
+    {
+        public int Number { get; set; }
+        public string Data { get; set; }
+        public float Sum { get; set; }
+
+
+        public Kvit()
+        {
+            Number = 0;
+            Data = "";
+            Sum = 0;
+        }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Ввод инфо о изделии: ");
+
+            Console.Write("Номер: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Дата: ");
+            Data = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Сумма: ");
+            Sum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Номер: " + Number);
+            Console.WriteLine("Дата: " + Data);
+            Console.WriteLine("Сумма: " + Sum);
         }
     }
 }
